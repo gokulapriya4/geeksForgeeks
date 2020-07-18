4e11291237973f18335775fa82b38279235dfc81
Gokula Priya <gokulapriya4@gmail.com>
2020-07-18 13:33:40 +0530

    adding problems

--- src/main/java/com/priya/geeksForGeeks/Accolite/IdentifyAndCorrectLoopInLinkedList.java
+++ src/main/java/com/priya/geeksForGeeks/Accolite/IdentifyAndCorrectLoopInLinkedList.java
@@ -1,4 +1,32 @@
 package main.java.com.priya.geeksForGeeks.Accolite;
 
-public class IdentifyAndCorrectLoopInLinkedList {
+import java.util.HashMap;
+
+public class IdentifyAndCorrectLoopInLinkedList extends LinkedListBase {
+
+    HashMap<Node, Node> visited = new HashMap<>();
+
+    public boolean detectLoop(Node head) {
+        Node node = head.next;
+        while (node != null) {
+            if (visited.containsKey(node))
+                return true;
+            else
+                visited.put(node, node);
+            node = node.next;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        IdentifyAndCorrectLoopInLinkedList obj = new IdentifyAndCorrectLoopInLinkedList();
+
+        obj.head = new Node(null, null);
+        obj.addNode(new Node(1, null));
+        obj.addNode(new Node(2, null));
+        obj.addNode(new Node(3, null));
+        obj.head.next.next.next.next = obj.head;
+
+        System.out.println(obj.detectLoop(obj.head));
+    }
 }
